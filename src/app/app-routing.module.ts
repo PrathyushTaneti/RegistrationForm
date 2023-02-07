6a82579385edfdd09e31df722fbd3c644554973e
Prathyush Taneti <prathyushtaneti.11@gmail.com>
2023-02-07 20:28:52 +0530

    Registration Form Draft 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { ReportComponent } from './report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'registration',
+  },
+  {
+    path: 'registration',
+    component: RegistrationFormComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'form',
+      },
+      {
+        path: 'form',
+        component: FormComponent,
+      },
+      {
+        path: 'report',
+        component : ReportComponent
+      },
+      {
+        path: '**',
+        component: FormComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: RegistrationFormComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { FormComponent } from './form/form.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent,
+    FormComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
